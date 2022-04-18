d16e99c2714d07cfb3afdd04bab5042605263cf1
Bernd Stransky <bstransky@newrelic.com>
2022-04-18 21:40:16 +0200

    webpack config changes for css and css mdolues

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,22 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
+              importLoaders: 1,
               modules: true,
             }
           }
         ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          }
+        ],
+        exclude: /\.module\.css$/,
       }
     ]
   },
